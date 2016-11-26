b6173558ab950d975cf49ecb66d3e181da06d8a5
12386716 <gavin.heaney@ucdconnect.ie>
2016-11-26 14:20:50 +0000

    New get request for Italian food

--- index.js
+++ index.js
@@ -49,6 +49,36 @@ app.get('/recipe', function(request, response) {
   })
 });
 
+app.get('/italian', function(request, response) {
+  unirest.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/searchComplex?addRecipeInformation=false&cuisine=italian&fillIngredients=false&includeIngredients=onions%2C+lettuce%2C+tomato%2C+pasta&intolerances=peanut&limitLicense=false&maxCalories=1500&maxCarbs=100&maxFat=100&maxProtein=100&minCalories=150&minCarbs=5&minFat=5&minProtein=5&number=10&offset=0&ranking=1&type=main+course")
+  .header("X-Mashape-Key", "UhgpDYqy2pmsh8nnaEksOhY83DJ2p1PHdyfjsnjmKT2rQVIH6S")
+  .header("Accept", "application/json")
+  .end(function (result) {
+
+  	var titles = [];
+
+  	console.log("result.status is:\n\n")
+  	console.log(result.status)
+
+  	console.log("result.headers is:\n\n")
+    console.log(result.headers)
+
+    console.log("result.body is:\n\n")
+  	console.log(result.body)
+
+    	console.log("recipe titles:")
+    	result.body.results.forEach(function(recipe){
+    		console.log(recipe)
+    		titles = titles.concat(recipe.title)
+    	});
+
+    response.render('pages/recipes', {
+    		recipeTitles: titles
+
+    });
+  })
+});
+
 app.listen(app.get('port')	, function() {
   console.log('Node app is running on port', app.get('port'));
 });
