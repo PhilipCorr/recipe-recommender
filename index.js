4b2d1bc4b1f8debd98c8dfbdcf706d9257b1d83d
12318551 <caroline.mc-mahon@ucdconnect.ie>
2016-12-11 17:54:51 +0000

    New Homepage and images

--- index.js
+++ index.js
@@ -19,34 +19,43 @@ app.get('/cool', function(request, response) {
   response.send(cool());
 });
 
-app.get('/recipe', function(request, response) {
+app.get('/homepage', function(request, response) {
+  response.render('pages/homepage', {
+       recipe_type: "",
+        recipeTitles: "",
+        recipeImages: ""
+
+    });
+});
+
+app.get('/american', function(request, response) {
   unirest.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/searchComplex?addRecipeInformation=false&cuisine=american&excludeIngredients=coconut%2C+mango&fillIngredients=false&includeIngredients=onions%2C+lettuce%2C+tomato&intolerances=peanut%2C+shellfish&limitLicense=false&maxCalories=1500&maxCarbs=100&maxFat=100&maxProtein=100&minCalories=150&minCarbs=5&minFat=5&minProtein=5&number=10&offset=0&query=burger&ranking=1&type=main+course")
   .header("X-Mashape-Key", "UhgpDYqy2pmsh8nnaEksOhY83DJ2p1PHdyfjsnjmKT2rQVIH6S")
   .header("Accept", "application/json")
   .end(function (result) {
 
-  	var titles = [];
+    var titles = [];
     var images =[];
 
-  	console.log("result.status is:\n\n")
-  	console.log(result.status)
+    console.log("result.status is:\n\n")
+    console.log(result.status)
 
-  	console.log("result.headers is:\n\n")
+    console.log("result.headers is:\n\n")
     console.log(result.headers)
 
     console.log("result.body is:\n\n")
-  	console.log(result.body)
+    console.log(result.body)
 
-    	console.log("recipe titles:")
-    	result.body.results.forEach(function(recipe){
-    		console.log(recipe)
-    		titles = titles.concat(recipe.title)
+      console.log("recipe titles:")
+      result.body.results.forEach(function(recipe){
+        console.log(recipe)
+        titles = titles.concat(recipe.title)
         images = images.concat(recipe.image)
-    	});
+      });
 
     response.render('pages/recipes', {
-       recipe_type: "",
-    		recipeTitles: titles,
+       recipe_type: "American",
+        recipeTitles: titles,
         recipeImages: images
 
     });
