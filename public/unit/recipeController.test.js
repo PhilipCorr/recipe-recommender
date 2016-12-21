725072b38064fb671c15051b65d75a01c2f21330
12386716 <gavin.heaney@ucdconnect.ie>
2016-12-21 15:27:46 +0000

    Test for removing diet filters

--- public/unit/recipeController.test.js
+++ public/unit/recipeController.test.js
@@ -38,16 +38,39 @@ describe('tests for recipeController', function () {
         it("Removing cuisine filters", function () {
             var newFilters =
             {
-                "cuisine": ["irish"],
+                "cuisine": ["irish", "mexican"],
                 "diet": ["vegan"],
                 "number": 10,
                 "includeIngredients": "potato"
             };
             scope.filters = newFilters;
             expect(scope.filters.cuisine).toContain("irish");
+            expect(scope.filters.cuisine).toContain("mexican");
+            console.log("scope.filters.cuisine: " + scope.filters.cuisine);
 
             scope.removeFilters(scope.filters.cuisine);
+            console.log("scope.filters.cuisine: " + scope.filters.cuisine);
             expect(scope.filters.cuisine).toBeEmptyArray;
+        });
+    });
+
+     describe('$scope.removeFilters', function(){
+        it("Removing diet filters", function () {
+            var newFilters =
+            {
+                "cuisine": ["irish"],
+                "diet": ["vegan", "paleo"],
+                "number": 10,
+                "includeIngredients": "potato"
+            };
+            scope.filters = newFilters;
+            expect(scope.filters.diet).toContain("vegan");
+            expect(scope.filters.diet).toContain("paleo");
+            console.log("scope.filters.diet: " + scope.filters.diet);
+
+            scope.removeFilters(scope.filters.diet);
+            console.log("scope.filters.diet: " + scope.filters.diet);
+            expect(scope.filters.diet).toBeEmptyArray;
         });
     });
 
