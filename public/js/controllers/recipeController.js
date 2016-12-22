d464dc4c767754066f14b19fddb5011842bbb6a6
12318551 <caroline.mc-mahon@ucdconnect.ie>
2016-12-22 16:10:29 +0000

    Popup to indicate ingredients have been added

--- public/js/controllers/recipeController.js
+++ public/js/controllers/recipeController.js
@@ -60,6 +60,9 @@ recipeApp.controller('recipeController', ['$scope', 'recipeService', 'listServic
 				
 			});
 		},
+		$scope.alert_added_ingredients = function(){
+			alert('Your ingredients have been added to the Shopping List!');
+		},
 		$scope.getDetailedData = function(id){
 			console.log("getDetailedData method in recipeController now executing...")
 			recipeService.getDetailedData(id)
