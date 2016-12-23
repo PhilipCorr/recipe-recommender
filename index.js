aa5feb50abd0cb406470045c7a41ca620d979ae4
Philip Corr <corrphilip@gmail.com>
2016-12-23 10:53:57 +0000

    partial bug fix for page reload

--- index.js
+++ index.js
@@ -8,11 +8,27 @@ app.use(express.static(__dirname + '/public'));
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 
-app.get('/', (req, res) => {
+app.get('/index/details', (req, res) => {
    res.sendFile('index.html', {
      root: 'views/pages'
    });
 });
+
+app.get('*', (req, res) => {
+   res.sendFile('index.html', {
+     root: 'views/pages'
+   });
+});
+
+// app.use(function(req, res) {
+//     res.sendfile('index.html', {
+//      root: 'views/pages'
+//    });
+// });
+
+// app.route('/*').get(function(req, res) { 
+//     return res.sendFile(path.join(config.root, 'index.html')); 
+// });
 
 app.listen(app.get('port')  , function() {
   console.log('Node app is running on port', app.get('port'));
